c06320f13e0f3f6efd77bce18e13a8ee2e18d5e5
jason <jjmaldonis@gmail.com>
2017-11-22 18:41:31 -0600

    updated the example

--- examples/match_history_roles.py
+++ examples/match_history_roles.py
@@ -1,7 +1,15 @@
+import cassiopeia as cass
 from cassiopeia import Summoner
 
 from roleidentification.get_roles import get_data
 from roleidentification.utilities import get_team_roles
+
+config = cass.get_default_config()
+config['pipeline']['ChampionGG'] = {
+    "package": "cassiopeia_championgg",
+    "api_key": "CHAMPIONGG_KEY"  # Your api.champion.gg API key (or an env var containing it)
+  }
+cass.apply_settings(config)
 
 
 def main():
